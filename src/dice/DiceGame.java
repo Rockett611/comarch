f381f68811b0bcd471aecc7bf46dc2d6e8b63036
Podnajmer Rafal <r.podnajmer@gmail.com>
2023-10-02 15:34:07 +0200

    version 3 working

--- src/dice/DiceGame.java
+++ src/dice/DiceGame.java
@@ -1,6 +1,5 @@
 package dice;
 
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,13 +9,16 @@ public class DiceGame {
 
         int score = 0;
         int tempScore = 0;
-        while (tempScore < 20) {
+        while (tempScore < 30) {
             int answer = option();
             if (answer == 1) {
                 int rolled = roll();
                 if (rolled == 1) {
+                    System.out.println("wyrzuciłeś " + rolled);
                     System.out.println("straciłeś wszystkie punkty :(");
                     tempScore = 0;
+                } else if (rolled == 0) {
+                    System.out.println("kość wypadła za stół, rzuć jeszcze raz");
                 } else {
                     tempScore += rolled;
                     System.out.println("wyrzuciłeś " + rolled);
@@ -25,12 +27,10 @@ public class DiceGame {
 
             } else if (answer == 0) {
                 score = tempScore;
-                tempScore = 0;
                 System.out.println("Twój wynik to " + score);
                 break;
             } else {
                 System.out.println("wprowadź poprawną odpowiedź");
-                answer = option();
             }
         }
 
@@ -41,7 +41,7 @@ public class DiceGame {
         Random random = new Random();
         int diceThrow = random.nextInt(7);
 
-        while (diceThrow == 0) {
+        if (diceThrow == 0) {
             roll();
         }
 
@@ -52,32 +52,14 @@ public class DiceGame {
         Scanner input = new Scanner(System.in);
 
         System.out.println("rzucamy (1) czy pas (0)?: ");
+        String answer = input.next();
 
-        try {
-            int answer = input.nextInt();
-        } catch (InputMismatchException ime) {
-            System.out.println("musisz wprowadzić cyfrę 1 lub 0");
+        if (!answer.equals("1") && !answer.equals("0") && answer.isEmpty()) {
+            System.out.println("wybierz opcję 1 lub 0");
             option();
         }
 
-
-
-//
-//        try {
-//            if (answer == 1) {
-//                System.out.println("gramy");
-//                return answer;
-//            } else if (answer == 0) {
-//                System.out.println("pas");
-//                return answer;
-//            } else {
-//                System.out.println("podaj 1 albo 0");
-//                option();
-//            }
-//        } catch (InputMismatchException ime) {
-//            System.out.println("musisz wprowadzić cyfrę 1 lub 0");
-//            option();
-//        }
-//        return answer;
+//        input.close();
+        return Integer.valueOf(answer);
     }
 }
